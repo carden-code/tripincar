e6a52d1cecb3cf8af7f020eb4e8036e4b88e9a33
Vyacheslav <borisenkov89@yandex.ru>
2022-09-19 21:37:45 +0300

    add fields in order model

--- tripincar/orders/models.py
+++ tripincar/orders/models.py
@@ -1,4 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Order(models.Model):
-    pass
+    author = models.ForeignKey(
+        User,
+        verbose_name='Заказчик',
+        on_delete=models.CASCADE,
+        related_name='orders'
+    )
+    
+    address = models.CharField(
+        verbose_name='Адрес',
+        max_length=256,
+        blank=False
+    )
+    
+    telephone = models.CharField(
+        verbose_name='Телефон',
+        max_length=11,
+        unique=True,
+        blank=False
+    )
+
+    comment = models.CharField(
+        verbose_name='Комментарий',
+        max_length=256,
+        blank=True
+    )
