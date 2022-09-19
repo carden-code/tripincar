1d825b63f431f90256d5e5259299775e927b01fb
Vyacheslav <borisenkov89@yandex.ru>
2022-09-19 23:37:40 +0300

    add list_display admin

--- tripincar/orders/admin.py
+++ tripincar/orders/admin.py
@@ -1,1 +1,22 @@
 from django.contrib import admin
+
+from .models import Order
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'author',
+        'telephone',
+        'pub_date',
+        'update_date',
+        'city',
+        'airport',
+        'driver',
+        'status',
+    )
+    search_fields = ('airport',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+admin.site.register(Order, OrderAdmin)
--- tripincar/orders/models.py
+++ tripincar/orders/models.py
@@ -11,16 +11,35 @@ class Order(models.Model):
         related_name='orders'
     )
     
+    driver = models.ForeignKey(
+        User,
+        verbose_name='Водитель',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    
+    city = models.CharField(
+        verbose_name='Город',
+        max_length=128,
+        blank=False
+    )
+    
     address = models.CharField(
         verbose_name='Адрес',
         max_length=256,
         blank=False
     )
     
+    airport = models.CharField(
+        verbose_name='Аэропорт',
+        max_length=64,
+        blank=False
+    )
+    
     telephone = models.CharField(
         verbose_name='Телефон',
         max_length=11,
-        unique=True,
         blank=False
     )
 
@@ -29,3 +48,27 @@ class Order(models.Model):
         max_length=256,
         blank=True
     )
+    
+    status = models.BooleanField(
+        verbose_name='Статус заказа'
+    )
+    
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+    
+    update_date = models.DateTimeField(
+        verbose_name='Дата обновления',
+        auto_now=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+        ordering = ('-pub_date',)
+        
+    def __str__(self):
+        return f'{self.airport} -> {self.city}'
