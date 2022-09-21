57897c7b34767ea04309d501ada64bf032bc15b3
Vyacheslav <borisenkov89@yandex.ru>
2022-09-21 22:11:40 +0300

    create app users

--- tripincar/tripincar/urls.py
+++ tripincar/tripincar/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include('orders.urls')),
+    path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')), 
+    path('', include('orders.urls')),
 
 ]
