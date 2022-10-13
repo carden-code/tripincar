c25f3e4a1663f58e3b958d99d81273efdedae0bc
Vyacheslav <borisenkov89@yandex.ru>
2022-10-13 23:38:02 +0300

    add handler errors in urls

--- tripincar/tripincar/urls.py
+++ tripincar/tripincar/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.csrf_failure'
+handler500 = 'core.views.server_error'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
