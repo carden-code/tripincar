3da531f5695b9701a57b87536c17cb08242244d3
Vyacheslav <borisenkov89@yandex.ru>
2022-10-14 00:11:48 +0300

    add fav anf logo

--- tripincar/tripincar/settings.py
+++ tripincar/tripincar/settings.py
@@ -7,13 +7,15 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(BASE_DIR / '.env')
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='123')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG') or True
+# DEBUG = False
 
-ALLOWED_HOSTS = []
+DEBUG = os.getenv('DEBUG') == 'True'
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
